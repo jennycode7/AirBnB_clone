a9f79e214faa368d19589e9233ac5ae27fd6d371
jennycode7 <jennessecosmas7@gmail.com>
2024-04-30 19:59:04 -0700

    AirBnB

--- models/base_model.py
+++ models/base_model.py
@@ -16,7 +16,7 @@ class BaseModel:
         '''
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
-        self.update_at = self.created_at
+        self.updated_at = self.created_at
 
     def __str__(self):
         '''
@@ -29,7 +29,7 @@ class BaseModel:
         '''
         updates self
         '''
-        self.update_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
         '''
@@ -38,5 +38,5 @@ class BaseModel:
         to_obj = self.__dict__.copy()
         to_obj['name'] = self.__class__.__name__
         to_obj['created_at'] = self.created_at.isoformat()
-        to_obj['updated_at'] = self.update_at.isoformat()
+        to_obj['updated_at'] = self.updated_at.isoformat()
         return to_obj
