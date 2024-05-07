9b620a60ca4aa1cc0a7dde9d2d59fd65d89a8001
jennycode7 <jennessecosmas7@gmail.com>
2024-05-07 07:12:12 -0700

    AirBnB

--- console.py
+++ console.py
@@ -5,21 +5,27 @@ A module
 import cmd
 
 class HBNBCommand(cmd.Cmd):
+    '''
+    A command prompt
+    '''
     prompt = '(hbnb) '
 
     def do_quit(self, arg):
-        'Quit command to exit the program'
+        '''Quit command to exit the program'''
         return True
     
     def emptyline(self):
+        '''For empty line and spaces'''
         pass
 
     
     def do_help(self, arg: str) -> bool | None:
+        ''' do help '''
         return super().do_help(arg)
     
 
     def do_EOF(self, arg):
+        '''For EOF characters'''
         return True
 
 
