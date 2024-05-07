20754a5bce3f555baa1791628915237cec457b0a
jennycode7 <jennessecosmas7@gmail.com>
2024-05-07 06:32:59 -0700

    AirBnB

--- console.py
+++ console.py
@@ -1,1 +1,24 @@
-print("hello world")
+#!/usr/bin/python3
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        'Quit command to exit the program'
+        return True
+    
+    def emptyline(self):
+        pass
+
+    
+    def do_help(self, arg: str) -> bool | None:
+        return super().do_help(arg)
+    
+
+    def do_EOF(self, arg):
+        return True
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
