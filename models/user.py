fc498f0c37f8b698997355cc509c490e4b8fc572
jennycode7 <jennessecosmas7@gmail.com>
2024-05-15 15:59:30 -0700

    AirBnB

--- models/user.py
+++ models/user.py
@@ -8,12 +8,7 @@ class User(BaseModel):
     '''
     A class
     '''
-    def __init__(self, *args, **kwargs):
-        '''
-        Initializer
-        '''
-        super().__init__(*args, **kwargs)
-        self.email = ""
-        self.password = ""
-        self.first_name = ""
-        self.last_name = ""
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
